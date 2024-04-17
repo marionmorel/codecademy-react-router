eb0ee81f06413c55abfadbc624d2500c4bb36e18
ma-mrl <marion.mrl@live.fr>
2024-04-17 12:20:51 +0200

    dynamic routes

--- src/app/App.js
+++ src/app/App.js
@@ -17,9 +17,11 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
     <Route path="/about" element={ <About/> } />
     <Route path="/sign-up" element={ <SignUp/> } />
-    <Route path="/articles" element={ <Article/> } />
+    <Route path="/articles" element={ <Articles/> } />
+    <Route path='/articles/:title' element={ <Article /> } />
     <Route path="/categories" element={ <Categories/> } />
-    <Route path="/profile" element={ <Profile/> } /> 
+    <Route path="/profile" element={ <Profile/> } />
+    <Route path="/authors/:author" element={ <Author/> } /> 
   </Route>
 ));
 
