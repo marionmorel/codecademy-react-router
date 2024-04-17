ac4a0fc0cf03062803f191b96b720a551782eb7d
ma-mrl <marion.mrl@live.fr>
2024-04-17 12:46:27 +0200

    small fix + useParams

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -23,7 +23,7 @@ export default function Articles () {
         { filteredArticles.map(article => (
           <li key={article.slug}>
             {/* Replace these a tags! */}
-            <Link to="./article">
+            <Link to={`${article.slug}`}>
               {article.title}
             </Link>
           </li>
